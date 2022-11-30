a68af9c5adff6b77eb3fe0a50c14915ba54cbf54
Joseph Moroney <josephmoroney97@gmail.com>
2022-11-30 20:56:34 +1000

    Cleanup base files; initial component work

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
+import { h } from "preact";
+import { setup, styled } from "goober";
+import { Koco } from "./Koco";
 
-export function App() {
-  const [count, setCount] = useState(0)
+const Background = styled("div")`
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`;
 
+setup(h);
+
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
-}
+    <Background>
+      <Koco />
+    </Background>
+  );
+};
