3f735614eb5a904938c0dedffdab74b947b83eef
Joseph Moroney <josephmoroney97@gmail.com>
2022-11-30 22:32:06 +1000

    Setup for date usage + override

--- src/Koco.jsx
+++ src/Koco.jsx
@@ -1,4 +1,19 @@
+import { useMemo } from "preact/hooks";
+import dayjs from "dayjs";
+
+const day = parseInt(dayjs().format("D"), 10);
+const month = parseInt(dayjs().format("M"), 10);
+const year = parseInt(dayjs().format("YYYY"), 10);
+
+const isAdventOrFuture = year > 2022 || (year === 2022 && month === 12);
+
 export const Koco = () => {
+  // Session storage var to override calendar
+  const force = useMemo(() => sessionStorage.getItem("FORCE_SHOW") === "1", []);
+
+  // Hide all if not advent/force show
+  if (!force && !isAdventOrFuture) return <></>;
+
   return (
     <svg
       width="100%"
