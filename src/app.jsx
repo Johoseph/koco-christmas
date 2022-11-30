75d1cf9870c43e1b03c52bd225068e7734a93993
Joseph Moroney <josephmoroney97@gmail.com>
2022-11-30 21:22:15 +1000

    Add basic animation based on mouse

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import { h } from "preact";
+import { useCallback, useEffect, useState } from "preact/hooks";
 import { setup, styled } from "goober";
 import { Koco } from "./Koco";
 
@@ -14,10 +15,41 @@ const Background = styled("div")`
 
 setup(h);
 
+const OFFSET_DIVISOR = 60;
+
 export const App = () => {
+  const [kocoOffset, setKocoOffset] = useState({ left: 0, top: 0 });
+
+  const handleMouseMove = useCallback((event) => {
+    const pageMiddle = {
+      width: document.body.clientWidth / 2,
+      height: document.body.clientHeight / 2,
+    };
+
+    const translateOffset = {
+      left: (pageMiddle.width - event.clientX) / OFFSET_DIVISOR,
+      top: (pageMiddle.height - event.clientY) / OFFSET_DIVISOR,
+    };
+
+    setKocoOffset(translateOffset);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
     <Background>
-      <Koco />
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          transform: `translate(${kocoOffset.left}px, ${kocoOffset.top}px)`,
+        }}
+      >
+        <Koco />
+      </div>
     </Background>
   );
 };
