f31d99f4dc5f9aab42a1ea80d513ef40448ab8ab
Joseph Moroney <josephmoroney97@gmail.com>
2022-12-05 22:34:59 +1000

    Only translate gamma

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ export const App = () => {
       translateOffset.top = currentTransform.top;
 
     if (transformRef.current.base)
-      transformRef.current.base.style.transform = `translate(${translateOffset.left}px, ${translateOffset.top}px)`;
+      transformRef.current.base.style.transform = `translate(${translateOffset.left}px, 0px)`;
   }, []);
 
   useEffect(() => {
